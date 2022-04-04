a8be734143f8945a4c0e95a5b9ba49487a980323
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-04 16:57:22 +0530

    Added Problems of Unit Conversion

---
+++ UnitConversion.js
@@ -0,0 +1,13 @@
+console.log("Unit Conversion \n")
+
+console.log("i) inch to feet conversion:- ")
+console.log("  1ft = 12inch");
+var a = 42 / 12;
+console.log("  42 inch = " + a + " feet \n");
+console.log("ii) Area of Rectangular Plot:-");
+console.log("   Dimensions = 60feet * 40feet");
+var area = 60 * 40;
+console.log("   Area = " + area + " sq. feet \n");
+console.log("iii) Area of such 25 plots");
+var area25 = area * 25;
+console.log("     Area of total plots = " + area25 + " sq. feet");
