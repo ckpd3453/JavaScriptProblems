d31950e15d5f3259136810ca5e1fd4daaaf8c2f0
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-04 15:42:41 +0530

    Added Problem to add two random dice number

---
+++ SumTwoRandomDiceNo.js
@@ -0,0 +1,6 @@
+const var1 = (Math.floor(Math.random() * 10) % 6) + 1;
+const var2 = (Math.floor(Math.random() * 10) % 6) + 1;
+var sumOfDiceNo = (var1 + var2);
+console.log("First Dice Number = " + var1);
+console.log("Second Dice Number = " + var2);
+console.log("Summation = " + sumOfDiceNo);
