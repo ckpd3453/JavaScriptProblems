a332f17e6ad47595a99b797fb893b0100ba23a0f
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-04 17:18:17 +0530

    Added Problem To find min max value of 5random numbers

---
+++ MinMaxOf5RandomNum.js
@@ -0,0 +1,9 @@
+const a = Math.floor(Math.random() * 999) + 100;
+const b = Math.floor(Math.random() * 999) + 100;
+const c = Math.floor(Math.random() * 999) + 100;
+const d = Math.floor(Math.random() * 999) + 100;
+const e = Math.floor(Math.random() * 999) + 100;
+
+console.log("5 Random Three Digits number are:- " + a + "," + b + "," + c + "," + d + "," + e);
+console.log("Minimum Value = " + Math.min(a, b, c, d, e));
+console.log("Maximum Value = " + Math.max(a, b, c, d, e));
