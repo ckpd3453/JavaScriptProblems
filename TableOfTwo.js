a907fa21f841b32a626e330d271aecc218518efa
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-06 09:03:52 +0530

    Added problem to print the table of 2 till n times using while loop

--- TableOfTwo.js
+++ TableOfTwo.js
@@ -1,10 +1,14 @@
-"use strict";
+const prompt = require('prompt-sync')();
 
-const reader = require("prompt-sync");
-const prompt = reader();
-
+//taking user input as constant
 const n = prompt("Enter the value of n : ");
-for (var i = 1; i <= n; i++) {
-    var table = 2 * i;
-    console.log("2 * " + i + " = " + table);
+var i = 1;
+while (i <= n) {
+    var mult = 2 * i;
+    if (mult <= 256) {
+        console.log("2 * " + i + " =" + " " + mult);
+    } else {
+        console.log("is Greater than 256");
+    }
+    i++;
 }
